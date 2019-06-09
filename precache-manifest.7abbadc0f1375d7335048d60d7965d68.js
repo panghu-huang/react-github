2dff5cde6f0a8924e066bffcb44ad1ca350c7a12
huangzhenjie <huangzhenjie@more.top>
2019-06-10 00:09:26 +0800

    Updates

--- precache-manifest.7abbadc0f1375d7335048d60d7965d68.js
+++ precache-manifest.7abbadc0f1375d7335048d60d7965d68.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6e3eeaea4ac1aafad0f1",
-    "url": "/react-github/static/css/main.512ff05a.chunk.css"
+    "revision": "c44f1077e9c0cf12a15a",
+    "url": "/react-github/static/css/main.e816ee76.chunk.css"
   },
   {
-    "revision": "6e3eeaea4ac1aafad0f1",
-    "url": "/react-github/static/js/main.6e3eeaea.chunk.js"
+    "revision": "c44f1077e9c0cf12a15a",
+    "url": "/react-github/static/js/main.c44f1077.chunk.js"
   },
   {
     "revision": "c7da51bda173be9f2f5c",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-github/static/js/runtime~main.67a2cc3a.js"
   },
   {
-    "revision": "00f2a1340cd115115dbcbc5b8b2e950c",
+    "revision": "067dd9470860d9d9bc836026decd7a67",
     "url": "/react-github/index.html"
   }
 ];
