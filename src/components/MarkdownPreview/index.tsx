acbe8a239f1f3352ad18e21493178339cd2eea5a
huangzhenjie <huangzhenjie@more.top>
2019-08-16 18:42:06 +0800

    markit 和 highlight.js 动态加载

--- src/components/MarkdownPreview/index.tsx
+++ src/components/MarkdownPreview/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
-import hljs from 'highlight.js'
-// @ts-ignore
-import markd from 'markit'
+import classNames from 'classnames'
 import { isFalse } from 'src/utils'
 import 'highlight.js/styles/github.css'
 import './style.scss'
@@ -12,18 +10,36 @@ interface IMarkdownPreviewProps {
   transform?: boolean
 }
 
-markd.setOptions({
-  highlight: (code: string) => {
-    return hljs.highlightAuto(code).value
-  }
-})
+const defaultMarkit = (markdown: string) => markdown
 
-const MarkdownPreview = ({ markdown, transform, className }: IMarkdownPreviewProps) => {
+const MarkdownPreview: React.FC<IMarkdownPreviewProps> = props => {
+  const markit = React.useRef(defaultMarkit)
+  const forceUpdate = React.useState(undefined)[1]
+  console.log(markit)
+  React.useEffect(() => {
+    if (!isFalse(props.transform)) {
+      const loadModules = async () => {
+        const hljs = (await import(/* webpackChunkName: "hl" */'highlight.js')).default
+        // @ts-ignore
+        const markd = (await import(/* webpackChunkName: "markit" */'markit')).default
+        markd.setOptions({
+          highlight: (code: string) => {
+            return hljs.highlightAuto(code).value
+          }
+        })
+        markit.current = markd
+        forceUpdate(undefined)
+      }
+      loadModules()
+    }
+  }, [props.transform])
   return (
     <div
-      className={`markdown-preview ${className}`}
+      className={classNames('markdown-preview', props.className)}
       dangerouslySetInnerHTML={{
-        __html: isFalse(transform) ? markdown : markd(markdown),
+        __html: isFalse(props.transform)
+          ? props.markdown :
+          markit.current(props.markdown),
       }}
     />
   )
